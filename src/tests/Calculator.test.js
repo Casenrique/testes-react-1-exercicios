87b50546004e2653e79607f3e2606bf12de1aa1f
Casenrique <carloshenriquesouza.eng@gmail.com>
2023-03-02 15:46:44 -0300

    Finalizado exercício 3 e o primeiro extra

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -4,7 +4,7 @@ import Calculator from "../components/Calculator"
 
 
 describe("Calculator", () => {
-    test("deve renderizar com o botão de soma", () => {
+    test("deve renderizar com o botão de dígitos de operações +, -, *, e /", () => {
         render(<Calculator />)
         screen.debug()
         // const sumButton = screen.getByText(/\+/i)
@@ -111,6 +111,12 @@ describe("Calculator", () => {
         const numberTwo = screen.getByRole('button', {
             name: /2/i
         })
+        const numberFour = screen.getByRole('button', {
+            name: /4/i
+        })
+        const divisionButton = screen.getByRole('button', {
+            name: /\//i
+        })
         const equalButton = screen.getByRole('button', {
             name: /=/i
         })
@@ -119,12 +125,62 @@ describe("Calculator", () => {
         await user.click(numberZero)
         await user.click(multiplicationButton)
         await user.click(numberTwo)
+        await user.click(divisionButton)
+        await user.click(numberFour)
         await user.click(equalButton)
 
         const value = screen.getByTestId("result")
         screen.logTestingPlaygroundURL()
         screen.debug()
-        expect(value).toHaveTextContent("20")
+        expect(value).toHaveTextContent("5")
+    })
+
+    test("deve renderizar os dígitos númericos renderizam corretamente", () => {
+        render(<Calculator />)
+        screen.debug()
+        // screen.logTestingPlaygroundURL()
+        const zeroButton = screen.getByRole('button', {
+            name: /0/i
+        })
+        const oneButton = screen.getByRole('button', {
+            name: /1/i
+        })
+        const twoButton = screen.getByRole('button', {
+            name: /2/i
+        })
+        const threeButton = screen.getByRole('button', {
+            name: /3/i
+        })
+        const fourButton = screen.getByRole('button', {
+            name: /4/i
+        })
+        const fiveButton = screen.getByRole('button', {
+            name: /5/i
+        })
+        const sixButton = screen.getByRole('button', {
+            name: /6/i
+        })
+        const sevenButton = screen.getByRole('button', {
+            name: /7/i
+        })
+        const eightButton = screen.getByRole('button', {
+            name: /8/i
+        })
+        const nineButton = screen.getByRole('button', {
+            name: /9/i
+        })
+             
+        expect(zeroButton).toBeInTheDocument()
+        expect(oneButton).toBeInTheDocument()
+        expect(twoButton).toBeInTheDocument()
+        expect(threeButton).toBeInTheDocument()
+        expect(fourButton).toBeInTheDocument()
+        expect(fiveButton).toBeInTheDocument()
+        expect(sixButton).toBeInTheDocument()
+        expect(sevenButton).toBeInTheDocument()
+        expect(eightButton).toBeInTheDocument()
+        expect(nineButton).toBeInTheDocument()
+       
     })
     
 })
